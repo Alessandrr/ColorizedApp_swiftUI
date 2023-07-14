abe9fde01eef5da4b16a71d3d48f20ecf028225b
Aleksandr Mamlygo <alexander.mamlygo@gmail.com>
2023-07-14 21:39:54 +0400

    Initial Commit

---
+++ ColorizedApp_swiftUI/ColorizedApp_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_swiftUIApp.swift
+//  ColorizedApp_swiftUI
+//
+//  Created by Александр Мамлыго on /147/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorizedApp_swiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ColorizedApp_swiftUI
+//
+//  Created by Александр Мамлыго on /147/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
